542045d4d984398e15d8e19192a7e2d36386d797
MaxRMNK <115977436+MaxRMNK@users.noreply.github.com>
2025-02-01 13:52:42 +0500

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'test-progressbar',
   plugins: [react()],
   // Чтобы не ругался на SCSS
   css: {
